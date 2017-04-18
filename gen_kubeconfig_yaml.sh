8869ff671a4b7267e718ceb3c09edc174d4b89de
esonchaiwanich <eric@galacticfog.com>
2017-04-18 16:49:56 -0400

    Made namespace a variable

--- gen_kubeconfig_yaml.sh
+++ gen_kubeconfig_yaml.sh
@@ -7,15 +7,16 @@ fi
 
 data=`base64 -w0 $1`
 outfile=kube_provider_config.yaml
+namespace=gestalt-system
 
 cat > $outfile << EOF
 # data encoded from $1
 kubeconfig: $data
 EOF
 
-echo "Encoded $1 to $outfile. "
+echo "Encoded kubernetes config file ($1) to $outfile. "
 echo ""
 echo "Pass with '-f' to helm install command, e.g."
 echo ""
-echo "    helm install --namespace gestalt-system ./gestalt -n gestalt-platform -f kube_provider_config.yaml"
+echo "    helm install --namespace $namespace ./gestalt -n gestalt-platform -f kube_provider_config.yaml"
 echo ""
