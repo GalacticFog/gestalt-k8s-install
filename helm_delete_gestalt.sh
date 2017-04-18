a192860980253ce1f6357ee4591c4ac4f63f0d1e
esonchaiwanich <eric@galacticfog.com>
2017-04-18 16:47:41 -0400

    Made namespace a variable

--- helm_delete_gestalt.sh
+++ helm_delete_gestalt.sh
@@ -1,17 +1,20 @@
+#!/bin/bash
+namespace=gestalt-system
+
 echo Deleting gestalt-platform ...
 helm delete --purge gestalt-platform
 echo Done.
 
 echo
 echo Listing PVCs...
-kubectl get pvc
+kubectl get pvc --namespace=$namespace
 
 echo
 echo Run to delete volume claims:
 echo
-echo "  kubectl delete pvc --all"
+echo "  kubectl delete pvc --all --namespace=$namespace"
 echo
-echo "Or, clean the entire namespace"
+echo "Or, clean the entire '$namespace' namespace"
 echo
-echo "  kubectl delete namespace gestalt-system"
+echo "  kubectl delete namespace $namespace"
 echo
