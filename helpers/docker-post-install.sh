4e72ad8f7dd8599edf0b3927aff696814e947f55
Eric Sonchaiwanich <eric@galacticfog.com>
2018-03-05 14:31:15 -0500

    New laser params, nodeports, move images to release-1.5.0

--- helpers/docker-post-install.sh
+++ helpers/docker-post-install.sh
@@ -1,6 +1,6 @@
 # This file is sourced into the installer
 
 # Form the Gateway URL
-kong_namespace=$(kubectl get svc --all-namespaces -ojsonpath='{.items[?(@.metadata.name=="default-kong")].metadata.namespace}')
-kong_port=$(kubectl get svc -n $kong_namespace default-kong -ojsonpath='{.spec.ports[?(@.name=="public-url")].nodePort}')
+kong_namespace=$(kubectl get svc --all-namespaces -ojsonpath='{.items[?(@.metadata.name=="kng")].metadata.namespace}')
+kong_port=$(kubectl get svc -n $kong_namespace kng -ojsonpath='{.spec.ports[?(@.name=="public-url")].nodePort}')
 GESTALT_GATEWAY_URL=$EXTERNAL_GATEWAY_PROTOCOL://$EXTERNAL_GATEWAY_HOST:$kong_port
