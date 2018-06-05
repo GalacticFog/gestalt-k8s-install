104fade2f073ae5f00b4b5bf95ce13e321128569
Eric Sonchaiwanich <eric@galacticfog.com>
2018-06-05 17:39:14 -0400

    Example resources, MIN_COOL=0 and multi-size executors

--- helpers/build-gestalt-cli-config.sh
+++ helpers/build-gestalt-cli-config.sh
@@ -25,7 +25,6 @@ cat - << EOF
     "laserImage" : "$gestalt_laser_image",
 	"laserCpu" : ${gestalt_laser_cpu-0.25},
 	"laserMem" : ${gestalt_laser_memory-1024},
-    "laserExecutorPort" : 60501,
     "laserMaxCoolConnectionTime" : 15,
     "laserExecutorHeartbeatPeriod" : 1000,
     "laserExecutorHeartbeatTimeout" : 1000,
@@ -63,7 +62,17 @@ if [ ! -z "$gestalt_laser_executor_js_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-js",
 				"runtime" : "nashorn",
 				"metaType" : "Nashorn",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
@@ -78,7 +87,17 @@ if [ ! -z "$gestalt_laser_executor_nodejs_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-nodejs",
 				"runtime" : "nodejs",
 				"metaType" : "NodeJS",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
@@ -93,7 +112,17 @@ if [ ! -z "$gestalt_laser_executor_jvm_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-jvm",
 				"runtime" : "java;scala",
 				"metaType" : "Java",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
@@ -108,7 +137,17 @@ if [ ! -z "$gestalt_laser_executor_dotnet_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-dotnet",
 				"runtime" : "csharp;dotnet",
 				"metaType" : "CSharp",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
@@ -123,7 +162,17 @@ if [ ! -z "$gestalt_laser_executor_python_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-python",
 				"runtime" : "python",
 				"metaType" : "Python",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
@@ -138,7 +187,17 @@ if [ ! -z "$gestalt_laser_executor_ruby_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-ruby",
 				"runtime" : "ruby",
 				"metaType" : "Ruby",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
@@ -153,7 +212,17 @@ if [ ! -z "$gestalt_laser_executor_golang_image" ]; then
 				"cmd" : "bin/gestalt-laser-executor-golang",
 				"runtime" : "golang",
 				"metaType" : "GoLang",
-                "extraEnv": {}
+        "extraEnv": {
+          "MIN_COOL": "0",
+          "SIZES_0_CPU": "0.1",
+          "SIZES_0_MEM": "128",
+          "SIZES_1_CPU": "0.2",
+          "SIZES_1_MEM": "256",
+          "SIZES_2_CPU": "0.5",
+          "SIZES_2_MEM": "512",
+          "SIZES_3_CPU": "1.0",
+          "SIZES_3_MEM": "1024"
+        }
 			}
 EOF
 fi
