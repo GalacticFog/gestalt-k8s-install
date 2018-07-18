8d70cb6e9372f2324be702d92bc78419eae0584c
Eric Sonchaiwanich <eric@galacticfog.com>
2018-07-18 13:27:15 -0400

    Better logging, check for docker pull auth issue when using email instead of docker ID

--- helpers/docker-pre-install.sh
+++ helpers/docker-pre-install.sh
@@ -2,14 +2,33 @@ image_pull_success=0
 
 pull_image() {
   # echo Pulling $1...
-  docker pull $1
+  output=$( docker pull $1 2>&1 )   # Note: 'local' doesn't work here - it causes the return code to be 0, and doesn't capture stdin
   if [ $? -ne 0 ]; then
+
+    echo "$output"
+
+    # Check for known issue:
+    echo "$output" | grep "unauthorized: incorrect username or password" > /dev/null
+    if [ $? -eq 0 ]; then
+      # It's possibly a known issue: https://github.com/docker/hub-feedback/issues/935
+      echo
+      echo "  Error - It appears that your Docker credentials are incorrect."
+      echo "  You may be logged into docker using your email address instead of your Docker ID."
+      echo "  (Refer to issue https://github.com/docker/hub-feedback/issues/935)"
+      echo
+      echo "  Please either 'docker logout' or log into Docker again using your Docker ID, and try again."
+      echo
+      exit_with_error "Error pulling docker image, installation cannot continue."
+    fi
+
     echo
     echo "Error pulling image '$1', trying one more time..."
     docker pull $1
     if [ $? -ne 0 ]; then
       exit_with_error "Error pulling docker image, installation cannot continue."
     fi
+  else
+    echo "$output"
   fi
   echo
 }
